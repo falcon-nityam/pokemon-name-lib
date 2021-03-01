f49ca0b8d17c64ae8d1d1f95715fe42147e0f9fb
falcon-nityam <nityambhangad0213@gmail.com>
2021-03-01 11:18:09 +0530

    write test for checking functionality

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,30 @@
 'use strict'
 
 var expect=require('chai').expect
+var pokemonNames=require('../src/index.js')
 
 describe('pokemon-names',()=>{
-    it('should work',()=>{
-        expect(true).to.be.true
+    it('should have a list of all pokemon names',()=>{
+        // pokemonNames.all
+        expect(isArrayOfString(pokemonNames.all)).to.be.true
+    })
+
+    it('should allow me to get a random names from the list',()=>{
+        // pokemonNames.unique
+        // expect(isIncludeIn(pokemonNames.all)(pokemonNames.unique())).to.be.true
+
+        expect(pokemonNames.unique()).to.satisfy(isIncludeIn(pokemonNames.all))
     })
 })
+
+function isArrayOfString(array){
+   return array.every(itm=>{
+        return typeof itm === 'string'
+    })
+}
+
+function isIncludeIn(array){
+    return (item)=>{
+        return array.indexOf(item) !== -1
+    }
+}
