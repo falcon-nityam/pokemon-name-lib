11f45f072370f13893fc179c1b804da5ca8ece7b
falcon-nityam <nityambhangad0213@gmail.com>
2021-03-06 11:33:42 +0530

    need to improve this�

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict"
 
-var uniqueRandomArray=require('unique-random-array')
-var pokemonCharacters=require('./pokemon-names.json')
+var uniqueRandomArray =require('unique-random-array')
+var pokemonCharacters =require('./pokemon-names.json')
 
 module.exports={
     all: pokemonCharacters,
